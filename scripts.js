81fb6731177db1bb4490c23f2a4e3c2afe5a724c
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-24 16:46:21 -0500

    Add event listeners for most buttons
    Add listener to number buttons that just update the text content of the input html class
    Input data storage is an array for easy adding of numbers to the end when another button is clicked
    Operator data storage starts as an empty string but will become the html class id string and will be used to decide what to do with the current results
    Clear event listener resets data storage and display to defaults

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+
+const inputDisplay = document.querySelector(".input");
+const resultDisplay = document.querySelector(".result");
+
+let result = null;
+let operator = '';
+let input = [];
+
 function add(augend, addend) {
     return augend + addend;
 }
@@ -13,3 +24,32 @@ function multiply(multiplier, multiplicand) {
 function divide(dividend, divisor) {
     return divisor != 0 ? dividend / divisor : alert("Do not divide by 0!");
 }
+
+function addEventListeners() {
+    numbers.forEach((button) => {
+        button.addEventListener("click", () => {
+            input.push(Number(button.id));
+            inputDisplay.textContent = input.join('');
+        });
+    });
+
+    operators.forEach((button) => {
+        button.addEventListener("click", () => {
+            operator = button.id;
+        });
+    });
+
+    clear.addEventListener("click", () => {
+        result = null;
+        operator = '';
+        input = [];
+        inputDisplay.textContent = '';
+        resultDisplay.textContent = '';
+    });
+}
+
+function init() {
+    addEventListeners();
+}
+
+init();
