a527be6957ca2eb5d29a770cf38117d703289b4c
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-24 15:48:31 -0500

    Add javascript for add, subtract, multiply and divide
    Names for function parameters based on this chart: https://en.wikipedia.org/wiki/Template:Arithmetic_operations
    Divide function checks for division by zero to prevent crashes

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function add(augend, addend) {
+    return augend + addend;
+}
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+function multiply(multiplier, multiplicand) {
+    return multiplier * multiplicand;
+}
+
+function divide(dividend, divisor) {
+    return divisor != 0 ? dividend / divisor : alert("Do not divide by 0!");
+}
