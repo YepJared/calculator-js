e142a5946c423ce3d42c10c890f5d2dcea16cd8f
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-25 16:56:22 -0500

    Complete calculator javascript to perform calculations
    Add Number to add, subtract, multiply, and divide functions as to not concatenate strings
    Add operate function to determine what to do with two numbers based on which operator was selected
    Update operator behavior to handle operations with or without the user pressing the equal sign

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal")
 
 const inputDisplay = document.querySelector(".input");
 const resultDisplay = document.querySelector(".result");
@@ -10,31 +11,74 @@ let operator = '';
 let input = [];
 
 function add(augend, addend) {
-    return augend + addend;
+    return Number(augend) + Number(addend);
 }
 
 function subtract(minuend, subtrahend) {
-    return minuend - subtrahend;
+    return Number(minuend) - Number(subtrahend);
 }
 
 function multiply(multiplier, multiplicand) {
-    return multiplier * multiplicand;
+    return Number(multiplier) * Number(multiplicand);
 }
 
 function divide(dividend, divisor) {
-    return divisor != 0 ? dividend / divisor : alert("Do not divide by 0!");
+    return divisor != 0 ? Number(dividend) / Number(divisor) : alert("Do not divide by 0!");
+}
+
+function updateInput(value) {
+    input.push(value);
+    inputDisplay.textContent = input.join('');
+}
+
+function updateResult(value) {
+    result = value;
+    resultDisplay.textContent = value;
+    input = [];
+    inputDisplay.textContent = '';
+}
+
+function operate() {
+    switch(operator) {
+        case 'plus':
+            result = add(result, input.join(''));
+            updateResult(result);
+            break;
+        case 'minus':
+            result = subtract(result, input.join(''));
+            updateResult(result);
+            break;
+        case 'times':
+            result = multiply(result, input.join(''));
+            updateResult(result);
+            break;
+        case 'divide':
+            result = divide(result, input.join(''));
+            updateResult(result);
+            break;
+        default:
+            break;
+    };
 }
 
 function addEventListeners() {
     numbers.forEach((button) => {
         button.addEventListener("click", () => {
-            input.push(Number(button.id));
-            inputDisplay.textContent = input.join('');
+            updateInput(button.id)
         });
     });
 
     operators.forEach((button) => {
         button.addEventListener("click", () => {
+            if (result === null) {
+                updateResult(input.join(''));
+            } else if (result !== null  && input.length != 0) {
+                if (operator != '') {
+                    operate();
+                } else {
+                    updateResult(input.join(''));
+                }
+            }
             operator = button.id;
         });
     });
@@ -45,6 +89,11 @@ function addEventListeners() {
         input = [];
         inputDisplay.textContent = '';
         resultDisplay.textContent = '';
+    });
+
+    equal.addEventListener("click", () => {
+        operate();
+        operator = '';
     });
 }
 
